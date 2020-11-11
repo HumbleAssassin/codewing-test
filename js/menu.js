ea054e16ed228b8ca662c308edfe255134fba863
Rajeev Thapa <thaparajeev1985@gmail.com>
2020-11-11 10:26:45 +0545

    second offering

--- js/menu.js
+++ js/menu.js
@@ -1,12 +1,12 @@
 let menu = document.getElementById('menu-items');
 let close = document.getElementsByClassName('fa-close')
 
-function openMenu(close) {
+function openMenu() {
     menu.classList.add('open');
     close.classList.add('show');
 }
 
-function closeMenu(close) {
+function closeMenu() {
     menu.classList.remove('open');
     close.classList.remove('show');
 }
